ee9b4b2b7c21f900587ab2169df54f4c2443e137
Royan <royanagist29@gmail.com>
2022-05-26 15:19:17 +0700

    remove console.log

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,5 @@ import '../styles/main.css';
 import personIcon from '../public/Person-placeholder.jpg';
 
 const personImages = document.querySelectorAll('#person');
-console.log(personImages);
 
 personImages.forEach((personImage) => { personImage.src = personIcon; });
