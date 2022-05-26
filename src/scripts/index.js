e2e9119a36966f9bf30ceccb3a249542d761e809
Royan <royanagist29@gmail.com>
2022-05-26 15:17:49 +0700

    repair team profile image, changing hero bg

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-param-reassign */
 import '../styles/main.css';
+import personIcon from '../public/Person-placeholder.jpg';
+
+const personImages = document.querySelectorAll('#person');
+console.log(personImages);
+
+personImages.forEach((personImage) => { personImage.src = personIcon; });
